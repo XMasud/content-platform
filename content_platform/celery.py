d445f1b299e3eb19983e20f01f466cafc6cbbd12
Masud Rana <masud.aiub.cse@gmail.com>
2026-01-26 16:13:23 +0100

    New normalization app created

---
+++ content_platform/celery.py
@@ -0,0 +1,10 @@
+import os
+from celery import Celery
+
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE', 'content-platform.settings' )
+
+app = Celery('content-platform')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
--- content_platform/settings.py
+++ content_platform/settings.py
@@ -40,7 +40,17 @@ INSTALLED_APPS = [
     'apps.ingestion',
     'drf_spectacular',
     'rest_framework',
+    'apps.normalization',
+    'django_celery_results'
 ]
+
+CELERY_BROKER_URL = "redis://localhost:6379/0"
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_BACKEND = "django-db"
+
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 300
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
